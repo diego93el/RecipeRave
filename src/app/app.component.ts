deabda102df28c1ecf16ea2a1924a2b8c7665897
diego93el <diego93el@gmail.com>
2024-02-05 12:21:56 +0100

    Search bar terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 import { CustomSidenavComponent } from "./components/custom-sidenav/custom-sidenav.component";
 
 @Component({
@@ -17,10 +19,26 @@ import { CustomSidenavComponent } from "./components/custom-sidenav/custom-siden
         MatToolbarModule,
         MatButtonModule,
         MatIconModule,
-        MatSidenavModule, CustomSidenavComponent]
+        MatSidenavModule, 
+        CustomSidenavComponent,
+        FormsModule,
+        MatInputModule
+      ]
 })
 export class AppComponent {
   title = 'RecipeRave';
+  items = [
+    'Elemento 1',
+    'Elemento 2',
+    'Elemento 3',
+    'Elemento 4'  
+  ]
+  filteredItems = [...this.items];
+  searchText : any;
+  applyFilter(event: KeyboardEvent) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.filteredItems = this.items.filter(item => item.toLowerCase().includes(filterValue.toLowerCase()));
+  }
   /**Metodo para la animacion de recogida del sidenav */
   collapsed = signal(false);
   /**Delimita el ancho del sidenav */
